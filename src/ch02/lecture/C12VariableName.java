e4dc1c773b91827324b5d64c9166d79095ff174b
jihoon0213 <jihun8138@gmail.com>
2025-04-28 16:44:30 +0900

    0428

---
+++ src/ch02/lecture/C12VariableName.java
@@ -0,0 +1,20 @@
+package ch02.lecture;
+
+public class C12VariableName {
+    public static void main(String[] args) {
+        // 영문대소문자,$,_,숫자
+        //숫자로 시작할 수 없음
+        // 의미있는 이름으로
+        // lowerCamelCase 사용
+        // 예약어(키워드)사용 불가
+
+        int modelName; // ok
+//        int class;
+//        int 6hour;
+        int $value; // 관습에 적합하지 않음
+        int _age; // 관습에 적합하지 않음
+//        int #name;
+//        int int;
+
+    }
+}
